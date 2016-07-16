37c842dacac82a03262c06ce588c129ec17bb84d
Matthew Ehrlich <mattehrlich86@gmail.com>
2016-07-16 16:16:29 -0700

    finished test

--- matt_practice_test.rb
+++ matt_practice_test.rb
@@ -3,7 +3,9 @@ require 'byebug'
 
 # This practice test was written by me, Matt Ehrlich. It should take about 45
 # minutes to complete. I didn't use rspec, just the poor man's rspec of having
-# tests print out true or false.
+# tests print out true or false. So, it's much easier to fool my tests than
+# rspec. Don't take shortcuts, follow the instructions carefully. To test your
+# code, just run score_test.rb
 
 # Challenge 1
 
@@ -35,6 +37,67 @@ end
 class Hash
 
   def my_each
+  end
+
+end
+
+# Challenge 3
+
+# Implement my_flatten. It should return take a nested array, and return a new
+# array with all of the elements of the original array and nested arrays.
+
+class Array
+
+  def my_flatten
+  end
+
+end
+
+# Challenge 4
+
+# implement subsets. It should return all subsets of an array.
+
+class Array
+
+  def subsets
+    []
+  end
+
+end
+
+# Challenge 5
+
+# Write a recursive function that will find the first 'num' elements of a set
+# that is defined by the following function: f(n) = n * 3 * f(n - 1) + 1. The base
+# case is f(0) = 1.
+
+def recursion_function(num)
+end
+
+# Challenge 6
+
+# Write a searching algorithm that searches for an element in a sorted array,
+# returning the index of that element. You must use binary search.
+
+class Array
+
+  def binary_search(el)
+
+  end
+
+end
+
+
+# Final Challenge
+
+# Implement quick_sort. Remember that quick_sort works by picking any element
+# of an array, then partitioning the array into two arrays, everything less
+# than that element, and everything greater than that element. It operates
+# recursively until the entire array is sorted.
+
+class Array
+
+  def quick_sort(&prc)
   end
 
 end
--- score_test.rb
+++ score_test.rb
@@ -33,7 +33,7 @@ else
 end
 
 puts "  Hash\#my_each"
-print "   Test1:"
+print "   Test:"
 test_hash = {'a' => 1, 'b' => 2, 'c' => 3, 'd' => 4}
 test_arr = []
 test_hash.my_each {|k, v| test_arr << [k, v]}
@@ -43,3 +43,93 @@ if test_arr == [['a', 1], ['b', 2], ['c', 3], ['d', 4]]
 else
   puts "FAILED!".colorize(:red)
 end
+
+puts "  my_flatten"
+print "   Test:"
+if [1, 2, [3, 4, [5], 6], 7, [8, 9]].my_flatten == [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  puts "PASSED".colorize(:green)
+  passed += 1
+else
+  puts "FAILED!".colorize(:red)
+end
+
+puts "  subsets"
+print "   Test 1:"
+if [].subsets == [[]]
+  puts "PASSED".colorize(:green)
+  passed += 1
+else
+  puts "FAILED!".colorize(:red)
+end
+
+print "   Test 2:"
+if ['a', 'b', 'c'].subsets.sort == [[], ['a'], ['b'], ['c'], ['a', 'b'], ['a', 'c'], ['b', 'c'], ['a', 'b', 'c']].sort
+  puts "PASSED".colorize(:green)
+  passed += 1
+else
+  puts "FAILED!".colorize(:red)
+end
+
+puts "  recursion function"
+print "   Test 1:"
+if recursion_function(1) == [1]
+  puts "PASSED".colorize(:green)
+  passed += 1
+else
+  puts "FAILED!".colorize(:red)
+end
+
+puts "    Test 2:"
+if recursion_function(5) == [1, 4, 25, 226, 2713]
+  puts "PASSED".colorize(:green)
+  passed += 1
+else
+  puts "FAILED!".colorize(:red)
+end
+
+puts "  binary search"
+print "   Test 1:"
+if [1, 7, 9, 12, 14, 16, 18].binary_search(14) == 4
+  puts "PASSED".colorize(:green)
+  passed += 1
+else
+  puts "FAILED!".colorize(:red)
+end
+
+print "   Test 2:"
+if [3, 9, 12, 14, 15, 18].binary_search(12) == 2
+  puts "PASSED".colorize(:green)
+  passed += 1
+else
+  puts "FAILED!".colorize(:red)
+end
+
+print "   Test 3:"
+if [2, 9, 7].binary_search(4).nil?
+  puts "PASSED".colorize(:green)
+  passed += 1
+else
+  puts "FAILED!".colorize(:red)
+end
+
+puts "  quick_sort"
+print "   Test 1:"
+if [7, 9, 3, 5, 4].quick_sort == [3, 4, 5, 7, 9]
+  puts "PASSED".colorize(:green)
+  passed += 1
+else
+  puts "FAILED!".colorize(:red)
+end
+
+print "   Test 2:"
+sort_test = [4, 9, 2, 1, 0, 8, 7, 7].quick_sort { |a, b| b <=> a }
+if sort_test == [9, 8, 7, 7, 4, 2, 1, 0]
+  puts "PASSED".colorize(:green)
+  passed += 1
+else
+  puts "FAILED!".colorize(:red)
+end
+
+puts "You scored #{passed} / 14"
+puts "You passed!" if passed >= 13
+puts "Sorry, you didn't pass" if passed < 13
