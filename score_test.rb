7edc6b1fc2c290f04e731c6b92e0df47c686b700
Matthew Ehrlich <mattehrlich86@gmail.com>
2016-07-16 17:04:07 -0700

    Fixed more bugs

--- score_test.rb
+++ score_test.rb
@@ -3,8 +3,8 @@ require_relative 'matt_practice_test'
 
 passed = 0
 puts
-puts "  my_each"
-print "    Test:"
+puts "my_each"
+print "  Test:"
 test1 = 0
 [1, 2, 3, 4, 5].my_each { |el| test1 += el }
 if test1 == 15
@@ -14,8 +14,8 @@ else
   puts "FAILED!".colorize(:red)
 end
 
-puts " my_map"
-print "   Test:"
+puts "my_map"
+print "  Test:"
 if [1, 2, 3, 4, 5].map(&:to_s) == [1, 2, 3, 4, 5].my_map(&:to_s)
   puts "PASSED!".colorize(:green)
   passed += 1
@@ -23,8 +23,8 @@ else
   puts "FAILED!".colorize(:red)
 end
 
-puts " my_select"
-print "   Test:"
+puts "my_select"
+print "  Test:"
 if [1, 2, 3, 4, 5].select(&:even?) == [1, 2, 3, 4, 5].my_select(&:even?)
   puts "PASSED".colorize(:green)
   passed += 1
@@ -32,8 +32,8 @@ else
   puts "FAILED!".colorize(:red)
 end
 
-puts "  Hash\#my_each"
-print "   Test:"
+puts "Hash\#my_each"
+print "  Test:"
 test_hash = {'a' => 1, 'b' => 2, 'c' => 3, 'd' => 4}
 test_arr = []
 test_hash.my_each {|k, v| test_arr << [k, v]}
@@ -44,8 +44,8 @@ else
   puts "FAILED!".colorize(:red)
 end
 
-puts "  my_flatten"
-print "   Test:"
+puts "my_flatten"
+print "  Test:"
 if [1, 2, [3, 4, [5], 6], 7, [8, 9]].my_flatten == [1, 2, 3, 4, 5, 6, 7, 8, 9]
   puts "PASSED".colorize(:green)
   passed += 1
@@ -53,8 +53,8 @@ else
   puts "FAILED!".colorize(:red)
 end
 
-puts "  subsets"
-print "   Test 1:"
+puts "subsets"
+print "  Test 1:"
 if [].subsets == [[]]
   puts "PASSED".colorize(:green)
   passed += 1
@@ -62,7 +62,7 @@ else
   puts "FAILED!".colorize(:red)
 end
 
-print "   Test 2:"
+print "  Test 2:"
 if ['a', 'b', 'c'].subsets.sort == [[], ['a'], ['b'], ['c'], ['a', 'b'], ['a', 'c'], ['b', 'c'], ['a', 'b', 'c']].sort
   puts "PASSED".colorize(:green)
   passed += 1
@@ -70,8 +70,8 @@ else
   puts "FAILED!".colorize(:red)
 end
 
-puts "  recursion function"
-print "   Test 1:"
+puts "recursion function"
+print "  Test 1:"
 if recursion_function(1) == [1]
   puts "PASSED".colorize(:green)
   passed += 1
@@ -79,7 +79,7 @@ else
   puts "FAILED!".colorize(:red)
 end
 
-print "    Test 2:"
+print "  Test 2:"
 if recursion_function(5) == [1, 4, 25, 226, 2713]
   puts "PASSED".colorize(:green)
   passed += 1
@@ -87,8 +87,8 @@ else
   puts "FAILED!".colorize(:red)
 end
 
-puts "  binary search"
-print "   Test 1:"
+puts "binary search"
+print "  Test 1:"
 if [1, 7, 9, 12, 14, 16, 18].binary_search(14) == 4
   puts "PASSED".colorize(:green)
   passed += 1
@@ -96,7 +96,7 @@ else
   puts "FAILED!".colorize(:red)
 end
 
-print "   Test 2:"
+print "  Test 2:"
 if [3, 9, 12, 14, 15, 18].binary_search(12) == 2
   puts "PASSED".colorize(:green)
   passed += 1
@@ -104,7 +104,7 @@ else
   puts "FAILED!".colorize(:red)
 end
 
-print "   Test 3:"
+print "  Test 3:"
 if [2, 9, 7].binary_search(4).nil?
   puts "PASSED".colorize(:green)
   passed += 1
@@ -112,8 +112,8 @@ else
   puts "FAILED!".colorize(:red)
 end
 
-puts "  quick_sort"
-print "   Test 1:"
+puts "quick_sort"
+print "  Test 1:"
 if [7, 9, 3, 5, 4].quick_sort == [3, 4, 5, 7, 9]
   puts "PASSED".colorize(:green)
   passed += 1
@@ -121,7 +121,7 @@ else
   puts "FAILED!".colorize(:red)
 end
 
-print "   Test 2:"
+print "  Test 2:"
 sort_test = [4, 9, 2, 1, 0, 8, 7, 7].quick_sort { |a, b| b <=> a }
 if sort_test == [9, 8, 7, 7, 4, 2, 1, 0]
   puts "PASSED".colorize(:green)
